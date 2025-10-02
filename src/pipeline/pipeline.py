7e95699c72e3d6fbfa00ac4a01d301a4a9e08303
anthony <anthonymhanna8@gmail.com>
2025-10-02 23:37:48 +0300

    FIxing image size

--- src/pipeline/pipeline.py
+++ src/pipeline/pipeline.py
@@ -29,7 +29,7 @@ def main():
     student.cuda()
 
     transform = transforms.Compose([
-        transforms.Resize([256, 256]),
+        transforms.Resize([224, 224]),
         transforms.ToTensor(),
         transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])
     ])
