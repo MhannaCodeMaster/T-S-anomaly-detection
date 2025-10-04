6f877b915d8b425f825597873152c9045fcd10be
anthony <anthonymhanna8@gmail.com>
2025-10-04 21:40:05 +0300

    Bug

--- src/pipeline/pipeline.py
+++ src/pipeline/pipeline.py
@@ -32,14 +32,14 @@ def main():
 
     # 1) Student (teacher–student) uses 256×256
     st_transform = transforms.Compose([
-        transforms.Resize([256, 256]),
+        transforms.Resize((256, 256)),
         transforms.ToTensor(),
         transforms.Normalize(mean=[0.485, 0.456, 0.406],
                             std =[0.229, 0.224, 0.225]),
     ])
 
     tl_transform = transforms.Compose([
-        transforms.Resize(224),
+        transforms.Resize((224,224)),
         transforms.ToTensor(),
         transforms.Normalize(mean=[0.485, 0.456, 0.406],
                             std =[0.229, 0.224, 0.225]),
